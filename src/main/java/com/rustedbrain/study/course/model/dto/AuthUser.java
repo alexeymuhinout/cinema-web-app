a8fcae2cb3e296badc0596fff7bf3651b2064c52
Alexey Muhin <alexeymuhinout@gmail.com>
2018-03-01 13:58:24 -0500

    Moving to MVP pattern, work in progress

---
+++ src/main/java/com/rustedbrain/study/course/model/dto/AuthUser.java
@@ -0,0 +1,29 @@
+package com.rustedbrain.study.course.model.dto;
+
+public class AuthUser {
+
+    private String name;
+    private UserRole userRole;
+
+    public AuthUser(String name, UserRole userRole) {
+        this.name = name;
+        this.userRole = userRole;
+    }
+
+    public String getLogin() {
+        return name;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    @Override
+    public String toString() {
+        return "RememberedUser{" +
+                "name='" + name + '\'' +
+                ", userRole=" + userRole +
+                '}';
+    }
+
+}
---
+++ src/main/java/com/rustedbrain/study/course/model/dto/UserRole.java
@@ -0,0 +1,10 @@
+package com.rustedbrain.study.course.model.dto;
+
+public enum UserRole {
+    NOT_AUTHORIZED,
+    ADMINISTRATOR,
+    MANAGER,
+    MEMBER,
+    MODERATOR,
+    PAYMASTER
+}
